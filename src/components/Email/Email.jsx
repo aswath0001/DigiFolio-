de603ad89769f05a9e3f3fdc749f966a7c97ad18
aswath0001 <rsaswaths38@gmail.com>
2025-02-27 16:18:53 +0530

    initial commit

---
+++ src/components/Email/Email.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Email = () => {
+  return (
+    <div>
+        <div className="container">
+            <div>
+                <p className='text-3xl md:text-4xl font-bold'>Join our email</p> 
+                <p>Get 20% offer per oder</p>
+            </div>
+            <div>
+                <button className='bg-[#B95134] text-black py-2 px-4 rounded shadow-sm hover:shadow-md'>
+                 Subscribe
+                 <div>
+                    
+                 </div>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Email
